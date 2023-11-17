0444822b7c6058b8019a66150baaff886bd7a889
ncarolinarojas <nataliarojasco22@gmail.com>
2023-11-17 17:38:57 -0500

    Organizacion de rutas

--- cr-pi-countries-main/server/src/routes/index.js
+++ cr-pi-countries-main/server/src/routes/index.js
@@ -3,3 +3,13 @@ const { Router } = require("express");
 const router = Router();
 
 module.exports = router;
+
+//router.get("/countries/:id", getCountryById);
+
+//router.get("/countries", getCountry);
+
+//router.post("/activity", postActivity);
+
+//router.get("/activity", getActivities);
+
+//router.delete("/activity", deleteActivity);
