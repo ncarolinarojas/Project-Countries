1658cbbb005c149b3e15a16b5041609d70f028c0
ncarolinarojas <nataliarojasco22@gmail.com>
2023-11-19 21:27:11 -0500

    Styles in nav bar

--- cr-pi-countries-main/UI_countries/src/components/NavBar/NavBar.jsx
+++ cr-pi-countries-main/UI_countries/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,15 @@
 import {NavLink} from 'react-router-dom'
+import '../../styles/navBar.scss'
+import logo from '../../assets/logo.svg'
 
 const NavBar = () => {
   return (
     <nav className='navbar'>
+        <div className='navbar__menu'>
+        <img src={logo} alt="logo" style={{maxWidth: '200px'}}/>
         <NavLink to={'/home'}> Home </NavLink>
         <NavLink to={'/createActivity'}> Create Activity</NavLink>
+        </div>
     </nav>
   )
 }
